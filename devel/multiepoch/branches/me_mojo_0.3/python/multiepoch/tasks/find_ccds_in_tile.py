b73504993284d13b2b5aeffa7e6502aac134dc6d
felipe <felipe@2422e80f-9519-0410-a819-8e21e7c86b6d>
2015-01-22 16:53:09 +0000

    more changes

--- devel/multiepoch/branches/me_mojo_0.3/python/multiepoch/tasks/find_ccds_in_tile.py
+++ devel/multiepoch/branches/me_mojo_0.3/python/multiepoch/tasks/find_ccds_in_tile.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
+import json
+import numpy
+import time
+import despyastro
+from mojo.jobs.base_job import BaseJob
+from despydb import desdbi
+from despyastro import tableio
+from despymisc.miscutils import elapsed_time
+npadd = numpy.core.defchararray.add
+
 """
 Finds all of the CCCs in the IMAGE table that fall inside the (RACMI,RACMAX)
 and (DECCMIN,DECCMAX) of a DES tile with a given set of extras SQL and/or
@@ -67,12 +77,6 @@ Author: Felipe Menanteau, NCSA, Nov 2014.
 
 """
 
-import json
-import numpy
-import despyastro
-from mojo.jobs.base_job import BaseJob
-from despydb import desdbi
-from despyastro import tableio
 
 # THE QUERY TEMPLATE THAT IS RUN TO GET THE CCDs
 # -----------------------------------------------------------------------------
@@ -128,34 +132,25 @@ class Job(BaseJob):
 
     def run(self):
 
-        # Get the db handle
-        if 'dbh' not in self.ctx:
-            try:
-                db_section = self.ctx.get('db_section','db-desoper')
-                print "# Creating db-handle to section: %s" % db_section
-                self.ctx.dbh = desdbi.DesDbi(section=db_section)
-            except:
-                raise ValueError('ERROR: Database handler could not be provided for context.')
-        else:
-            print "# Will recycle existing db-handle"
+
+        # Check for the db_handle
+        self.check_dbh()
 
         # Call the query built function
-        query = self.get_query(**self.ctx.get_kwargs_dict())
-        
         print "# Getting CCD images within the tile definition"
-        # Get the ccd images that are part of the DESTILE
-        #self.ctx.CCDS = despyastro.genutil.query2dict_of_columns(query,dbhandle=self.ctx.dbh,array=True)
-        self.ctx.CCDS = despyastro.genutil.query2rec(query,dbhandle=self.ctx.dbh)
-        print "# Nelem %s" % len(self.ctx.CCDS['FILENAME'])
+        self.get_CCDS(**self.ctx.get_kwargs_dict())
 
+        # Now we get the locations
+        self.get_fitsfile_locations()
+        
         # Get the filters we found
         self.ctx.BANDS  = numpy.unique(self.ctx.CCDS['BAND'])
         self.ctx.NBANDS = len(self.ctx.BANDS)
 
+    def get_CCDS(self,**kwargs): 
 
-    def get_query(self,**kwargs): 
         '''
-        Get the database query that returns the ccds.
+        Get the database query that returns the ccds and store them in a numpy record array
 
         kwargs
         ``````
@@ -193,10 +188,70 @@ class Job(BaseJob):
                 and_extras    = and_extras + ' AND\n (' + ' OR '.join(corners_and) + ')',
                 )
 
-        print query
-        return query
+        print "# Will execute the query:\n%s\n" %  query
+        # Get the ccd images that are part of the DESTILE
+        #self.ctx.CCDS = despyastro.genutil.query2dict_of_columns(query,dbhandle=self.ctx.dbh,array=True)
+        t0 = time.time()
+        self.ctx.CCDS = despyastro.genutil.query2rec(query,dbhandle=self.ctx.dbh)
+        print "# Query time: %s" % elapsed_time(t0)
+        print "# Nelem %s" % len(self.ctx.CCDS['FILENAME'])
+        return 
 
 
+    def get_fitsfile_locations(self):
+
+        """ Find the location of the files in the des archive and https urls"""
+
+        # Get all of the relevant kwargs
+        kwargs = self.ctx.get_kwargs_dict()
+        archive_name = kwargs.pop('archive_name', 'desar2home') # or get?
+
+        # Number of images/filenames
+        Nimages = len(self.ctx.CCDS['FILENAME'])
+
+        # 1. Figure out the archive root and root_http variables, it returns
+        # self.ctx.root_archive and self.ctx.root_https
+        self.get_root_archive(archive_name)
+
+        # 2. Construnct the relative path for each file as a combination of PATH + FILENAME
+        self.ctx.FILEPATH = npadd(self.ctx.CCDS['PATH'],"/")
+        self.ctx.FILEPATH = npadd(self.ctx.FILEPATH,self.ctx.CCDS['FILENAME'])
+
+        # 3. Create the archive locations for each file
+        path = [self.ctx.root_archive+"/"]*Nimages
+        self.ctx.FILEPATH_ARCHIVE = npadd(path,self.ctx.FILEPATH)
+
+        # 4. Create the https locations for each file
+        path = [self.ctx.root_https+"/"]*Nimages
+        self.ctx.FILEPATH_HTTPS   = npadd(path,self.ctx.FILEPATH)
+        return
+
+    def get_root_archive(self,archive_name='desar2home'):
+
+        """
+        Get the archive root and root_https fron the database
+        """
+
+        cur = self.ctx.dbh.cursor()
+        
+        # archive root
+        query = "select root from ops_archive where name='%s'" % archive_name
+        print "# Getting the archive root name for section: %s" % archive_name
+        print "# Will execute the SQL query:\n********\n** %s\n********" % query
+        cur.execute(query)
+        self.ctx.root_archive = cur.fetchone()[0]
+        print "# root_archive: %s" % self.ctx.root_archive
+
+        # root_https
+        query = "select val from ops_archive_val where name='%s' and key='root_https'" % archive_name
+        print "# Getting root_https for section: %s" % archive_name
+        print "# Will execute the SQL query:\n********\n** %s\n********" % query
+        cur.execute(query)
+        self.ctx.root_https = cur.fetchone()[0]
+        print "# root_https: %s" % self.ctx.root_https
+
+        cur.close()
+        return
 
     def write_info(self,ccdsinfo_file,names=['FILENAME','PATH','BAND','MAG_ZERO']):
 
@@ -211,7 +266,6 @@ class Job(BaseJob):
         tableio.put_data(ccdsinfo_file,tuple(variables), header=header, format=format)
         return
 
-
     def write_info_json(self,ccdsinfo_jsonfile,names=['FILENAME','PATH','BAND','MAG_ZERO']):
 
         print "# Writing CCDS information to: %s" % ccdsinfo_jsonfile
@@ -224,6 +278,20 @@ class Job(BaseJob):
         o.close()
         return
 
+    def check_dbh(self):
+
+        """ Check if we have a valid database handle (dbh)"""
+        
+        if 'dbh' not in self.ctx:
+            try:
+                db_section = self.ctx.get('db_section','db-desoper')
+                print "# Creating db-handle to section: %s" % db_section
+                self.ctx.dbh = desdbi.DesDbi(section=db_section)
+            except:
+                raise ValueError('ERROR: Database handler could not be provided for context.')
+        else:
+            print "# Will recycle existing db-handle"
+        return
 
     def __str__(self):
         return 'find ccds in tile'
@@ -248,9 +316,11 @@ def cmdline():
     # The positional arguments
     parser.add_argument("tileinfo", help="The json file with the tile information")
 
-    # Positional arguments
+    # Optional arguments
     parser.add_argument("--db_section", action="store", default="db-desoper",
                         help="DataBase Section to connect")
+    parser.add_argument("--archive_name", action="store", default="desar2home",
+                        help="DataBase Archive Name section")
     parser.add_argument("--select_extras", action="store", default=SELECT_EXTRAS,
                         help="string with extra SELECT for query")
     parser.add_argument("--and_extras", action="store", default=AND_EXTRAS,
@@ -269,10 +339,8 @@ def cmdline():
                         help="Output file name for plot")
     parser.add_argument("--tiledir", action="store", default="./", 
                         help="Path to Directory where we will write out plot the files ")
-
     args = parser.parse_args()
     return args
-
 
 if __name__ == "__main__":
 
@@ -284,7 +352,7 @@ if __name__ == "__main__":
     job = Job()
     # Add cmdline options to the context using Struct
     job.ctx = Struct(dict(**args.__dict__))
-    # Load in the json file
+    # Load in the info in the input json file
     json_dict = read_tileinfo(args.tileinfo)
     job.ctx.tileinfo =json_dict['tileinfo']
     job.ctx.tilename =json_dict['tilename']
@@ -292,13 +360,7 @@ if __name__ == "__main__":
     # Write out the ccds information
     #job.write_info(args.ccdsinfo)
     #job.write_info_json(args.ccdsinfo)
-    exit()
-
-    # Now we get the absolute paths in the archive 
-    from multiepoch.tasks.find_fitsfiles_location import Job as find_job    
-    find = find_job(ctx=job.ctx) 
-    find() # Get find.ctx.FILEPATH_ARCHIVE
-
+    #exit()
 
     # In Case we want to plot the overlapping CCDs
     if args.plot_overlap:
