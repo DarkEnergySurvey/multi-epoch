ac581fa5c8cdcdc0b66e1d00a0f1ac926aaf619e
Felipe Menanteau <menanteau@users.noreply.github.com>
2015-02-05 20:53:06 +0000

    delete duplicate code make_swarp_weight.py

--- bin/modify_weight_forSWarp.py
+++ bin/modify_weight_forSWarp.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from multiepoch.DESfits import DESFITS, elapsed_time
+from despymisc.miscutils import elapsed_time
 import time
 import sys
 
@@ -8,21 +9,18 @@ if __name__ == "__main__":
 
     # Get the start time
     t0 = time.time()
-    
-    fileName = sys.argv[1]
-    outName  = sys.argv[2]
-    clobber = False
+    try:
+        fileName = sys.argv[1]
+        outName  = sys.argv[2]
+    except:
+        usage = """
+        ERROR:
+        USAGE: %s <filename> <outname>
+        """ % os.path.basename(sys.argv[0])
+        sys.exit(usage)
 
-    desfits = DESFITS(fileName,outName,clobber=clobber)
-    #desfits.fix_weight_bleedtrail()
-    #desfits.replace_weightBit(maskbit=4)
-
+    desfits = DESFITS(fileName,outName,clobber=False)
     # Replace bleed trails 2^6 = 64
     desfits.replace_weightBit(maskbit=64)
-
-    # All interp pixels
-    #desfits.replace_weightBit(maskbit=4)
-
-    #desfits.write()
     desfits.write_weight()
-    print >>sys.stderr,"# Time:%s" % elapsed_time(t0)
+    print "# Done in %s\n" % elapsed_time(t0)
