6aa30eff7d26628a43b47d16fd102dd448699c07
felipe <felipe@2422e80f-9519-0410-a819-8e21e7c86b6d>
2015-02-05 20:47:18 +0000

    name change

--- devel/multiepoch/trunk/bin/make_swarp_weight.py
+++ devel/multiepoch/trunk/bin/make_swarp_weight.py
@@ -3,17 +3,24 @@
 import os,sys
 from multiepoch.DESfits import DESFITS
 from despymisc.miscutils import elapsed_time
+import time
 
 if __name__ == "__main__":
 
     # Get the start time
     t0 = time.time()
-    fileName = sys.argv[1]
-    outName  = sys.argv[2]
-    clobber = False
+    try:
+        fileName = sys.argv[1]
+        outName  = sys.argv[2]
+    except:
+        usage = """
+        ERROR:
+        USAGE: %s <filename> <outname>
+        """ % os.path.basename(sys.argv[0])
+        sys.exit(usage)
+
     desfits = DESFITS(fileName,outName,clobber=False)
     # Replace bleed trails 2^6 = 64
     desfits.replace_weightBit(maskbit=64)
     desfits.write_weight()
     print "# Done in %s\n" % elapsed_time(t0)
-    return 
