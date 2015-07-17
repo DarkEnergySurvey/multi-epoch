92b8c2b106cf4310bed77a3a9ea539ac76e67797
mgraber <mgraber@2422e80f-9519-0410-a819-8e21e7c86b6d>
2015-07-17 04:00:43 +0000

    documentation in config file improved.

--- devel/multiepoch/trunk/python/multiepoch/config/full_pipeline.py
+++ devel/multiepoch/trunk/python/multiepoch/config/full_pipeline.py
@@ -20,7 +20,7 @@ $ setup -r -v .
 ```
 
 from the multiepoch trunk directory after svn checkout
-then you can run this pipeline from anywhere by executing
+you can then run this pipeline from anywhere by executing
 
 $ mojo run_config multiepoch.config.full_pipeline
 
@@ -54,6 +54,7 @@ jobs = [
 
         'multiepoch.tasks.make_SWarp_weights',
         'multiepoch.tasks.call_SWarp_michael_dev',
+
 #       'multiepoch.tasks.call_SWarp_CustomWeights',
 #       'multiepoch.tasks.call_Stiff',
 #       'multiepoch.tasks.call_SExpsf',
@@ -65,47 +66,53 @@ jobs = [
 # SETTING UP THE PATHS
 # -----------------------------------------------------------------------------
 
-# MULTIEPOCH_ROOT ::
-# for default standalone applications we define one writeable root directory
-# in docker containers you will want to have this directory be a mounted volume
+# Only the REQUIRED PIPELINE PARAMETERS local_archive, weights_archive and
+# tiledir have to be set to run the pipeline. They CAN BE SET INDEPENDENTLY.
+# MULTIEPOCH_ROOT and outputpath are simply supportive, non-required
+# organisational variables.
+
+# MULTIEPOCH_ROOT :: not required organizational support variable
+# For default standalone applications we define one writeable root directory.
+# In docker containers you will want to have this directory to be a mounted
+# volume.
+# Required permissions for the executing user: writing
 MULTIEPOCH_ROOT = os.path.abspath('/MULTIEPOCH_ROOT')
 
-# local_archive ::
-# the path where your archive input images are located
-# either they are already present here or will be transferred to here
-# !! if left empty we assume you're in the cosmology cluster and set
-# local_archive accordingly (done in find_ccds_in_tile.py task)
+# local_archive :: REQUIRED PIPELINE PARAMETER !!
+# The path where your input archive images are located: Either they are already
+# present here or will be transferred to here using the get_fitsfiles task.
+# Required permissions for the executing user: only reading required
 local_archive = os.path.join(MULTIEPOCH_ROOT, 'LOCAL_DESAR')
 
-# weights_archive ::
-# the weights archive is a mirrored directory structure of the local_archive
+# weights_archive :: REQUIRED PIPELINE PARAMETER !!
+# The weights archive is a mirrored directory structure of the local_archive
 # and can be chosen to be the same as local_archive in case you would like your
 # weights files to end up in the 'archive'.
-# please be sure though that you have writing permissions therein!
+# Required permissions for the executing user: writing
 weights_archive = os.path.join(MULTIEPOCH_ROOT, 'LOCAL_WEIGHTS')
 
-# TODO : change local_archive to archive_path and weights_archive to
-# weights_path
-
-# outputpath ::
+# outputpath :: not required organizational support variable
+# Required permissions for the executing user: writing
 outputpath = os.path.join(MULTIEPOCH_ROOT, 'TILEBUILDER') 
 
-# tiledir ::
-# the directory where ultimately all output will end up for the tile being
+# tiledir :: REQUIRED PIPELINE PARAMETER !!
+# The directory where ultimately all output will end up for the tile being
 # produced here.
+# Required permissions for the executing user: writing
 tiledir = os.path.join(outputpath, tilename)
-
-# at the end of a run of this task we persist some of the ctx to be able to
-# reuse the generated information in later executable runs
-#json_dump_file = os.path.join(tiledir, tilename+'_tilename_tileinfo.json')
-#dump_var_list = ['tilename', 'tileinfo', 'assoc',]
-
 
 
 # GENERIC COMPUTATIONAL SETTINGS 
 # -----------------------------------------------------------------------------
 NTHREADS = 8
 NCPU = 8
+
+
+# LOGGING
+# -----------------------------------------------------------------------------
+stdoutloglevel = 'DEBUG'
+fileloglevel = 'DEBUG'
+logfile = os.path.join(tiledir, tilename+'_full_pipeline.log')
 
 
 # JOB SPECIFIC CONFIGURATION
@@ -116,24 +123,25 @@ coaddtile_table = 'felipe.coaddtile_new'
 db_section = 'db-destest'
 desservicesfile = '/MULTIEPOCH_ROOT/.desservices.ini'
 
-# set_tile_directory >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# -
 
 # find_ccds_in_tile >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 tagname = 'Y2T_FIRSTCUT'
 exec_name = 'immask'
 
+
 # plot_ccd_corners_destile >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# no extra config
+# -
+
 
 # get_fitsfiles >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# FIXME !! what's filepath_local good for???
 http_section = 'http-desarchive'
+
 
 # make_SWarp_weights >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 clobber_weights = False
 MP_weight = NCPU
 weights_execution_mode = 'execute'
+
 
 # call_SWarp >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 swarp_parameters = {
@@ -143,10 +151,3 @@ swarp_parameters = {
     }
 DETEC_COMBINE_TYPE = "CHI-MEAN"
 swarp_execution_mode = EXECUTION_MODE
-
-
-# LOGGING
-# -----------------------------------------------------------------------------
-stdoutloglevel = 'DEBUG'
-fileloglevel = 'DEBUG'
-logfile = os.path.join(tiledir, tilename+'_full_pipeline.log')
