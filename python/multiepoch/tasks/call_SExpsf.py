d67ea73e61e69bccda5cc64692b633db6fee7be3
Felipe Menanteau <menanteau@users.noreply.github.com>
2015-09-01 20:25:21 +0000

    commit for band usage

--- python/multiepoch/tasks/call_SExpsf.py
+++ python/multiepoch/tasks/call_SExpsf.py
@@ -39,9 +39,10 @@ class Job(BaseJob):
         assoc      = Dict(None,help="The Dictionary containing the association file",argparse=False)
         assoc_file = CUnicode('',help="Input association file with CCDs information",input_file=True,
                               argparse={ 'argtype': 'positional', })
+        tilename   = Unicode(None, help="The Name of the Tile Name to query",
+                             argparse={ 'argtype': 'positional', })
 
         # Optional Arguments
-        tilename    = Unicode(None, help="The Name of the Tile Name to query")
         tilename_fh = CUnicode('',  help="Alternative tilename handle for unique identification default=TILENAME")
         tiledir     = Unicode(None, help='The output directory for this tile.')
         SExpsf_execution_mode  = CUnicode("tofile",help="SEx for psfex excution mode",
