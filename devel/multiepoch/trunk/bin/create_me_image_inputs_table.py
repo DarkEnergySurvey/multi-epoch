aae19d1c5b5ad01528ff45faff2927c8f60ec532
felipe <felipe@2422e80f-9519-0410-a819-8e21e7c86b6d>
2016-03-02 21:33:09 +0000

    updated query for create_me_image_inputs_table.py and create_me_catalog_inputs_table.py

--- devel/multiepoch/trunk/bin/create_me_image_inputs_table.py
+++ devel/multiepoch/trunk/bin/create_me_image_inputs_table.py
@@ -17,21 +17,18 @@ create table {tablename_root}_{tagname} as
          file_archive_info.FILENAME,
 	 file_archive_info.COMPRESSION,
          file_archive_info.PATH,
-         wgb.UNITNAME,wgb.REQNUM,wgb.ATTNUM,
+         image.PFW_ATTEMPT_ID,
          image.BAND,
          image.CCDNUM,
 	 image.RA_CENT,image.DEC_CENT,
          image.RAC1,  image.RAC2,  image.RAC3,  image.RAC4,
          image.DECC1, image.DECC2, image.DECC3, image.DECC4
      FROM
-         wgb, ops_proctag, image, file_archive_info
+         ops_proctag, image, file_archive_info
      WHERE
          file_archive_info.FILENAME  = image.FILENAME AND
-         file_archive_info.FILENAME  = wgb.FILENAME  AND
-         wgb.FILETYPE    = '{filetype}' AND
-         wgb.REQNUM      = ops_proctag.REQNUM AND
-         wgb.UNITNAME    = ops_proctag.UNITNAME AND
-         wgb.ATTNUM      = ops_proctag.ATTNUM AND
+	 image.PFW_ATTEMPT_ID = ops_proctag.PFW_ATTEMPT_ID AND
+         image.FILETYPE  = '{filetype}' AND
          ops_proctag.TAG = '{tagname}'
 """
 
