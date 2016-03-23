ce64d36a8c8fae2f9cb1a23e4790f0869dfeacf2
felipe <felipe@2422e80f-9519-0410-a819-8e21e7c86b6d>
2016-03-23 16:14:31 +0000

    merged call_scamp_scampcats.py back to call_scamp.py

--- devel/multiepoch/trunk/python/multiepoch/tasks/call_scamp.py
+++ devel/multiepoch/trunk/python/multiepoch/tasks/call_scamp.py
@@ -24,7 +24,7 @@ from mojo.utils import log as mojo_log
 # JOB INTERNAL CONFIGURATION
 SCAMP_EXE = 'scamp'
 DETNAME = 'det'
-BKLINE = "\\\n"
+BKLINE = " \\\n"
 class Job(BaseJob):
 
     class Input(IO):
@@ -37,6 +37,13 @@ class Job(BaseJob):
         catlist      = Dict(None,help="The Dictionary containing input CCD-level catalog list ",argparse=False)
         cats_file    = CUnicode('',help="Name of the output ASCII catalog list storing the information for scamp", input_file=True,
                                 argparse={ 'argtype': 'positional', })
+
+        scampcats_file  = CUnicode('',help="Name of the output ASCII catalog list storing the information for scampcats", input_file=True)
+        scampcatlist    = Dict({},help="The Dictionary containing scampcat catalog list ",argparse=False)
+
+        scampheads_file = CUnicode('',help="Name of the output ASCII catalog list storing the information for scampheads", input_file=True)
+        scampheadlist   = Dict({},help="The Dictionary containing scamphead catalog list ",argparse=False)
+
 
         ######################
         # Optional arguments
@@ -55,6 +62,9 @@ class Job(BaseJob):
                                 argparse={'nargs':'+',})
         scamp_conf = CUnicode(help="Optional scamp configuration file")
 
+        # Use scampcats
+        use_scampcats = Bool(False, help=("Use finalcut scampcats for super-alignment[Default=False]"))
+
         # Logging -- might be factored out
         stdoutloglevel = CUnicode('INFO', help="The level with which logging info is streamed to stdout",
                                   argparse={'choices': ('DEBUG','INFO','CRITICAL')} )
@@ -67,12 +77,35 @@ class Job(BaseJob):
             mydict['catlist'] = {col: df[col].values.tolist() for col in df.columns}
             return mydict
 
+        def _read_scampcats_file(self):
+            mydict = {}
+            df = pd.read_csv(self.cats_file,sep=' ')
+            mydict['scampcatlist'] = {col: df[col].values.tolist() for col in df.columns}
+            return mydict
+
+        def _read_scampheads_file(self):
+            mydict = {}
+            df = pd.read_csv(self.cats_file,sep=' ')
+            mydict['scampheadlist'] = {col: df[col].values.tolist() for col in df.columns}
+            return mydict
+
         def _validate_conditional(self):
 
             # Get logger
             logger = mojo_log.get_logger({})
             if self.tilename_fh == '':
                 self.tilename_fh = self.tilename
+
+            # Check for valid input scampcats_file
+            if self.mojo_execution_mode == 'job as script' and self.use_scampcats and self.scampcats_file == "":
+                mess = 'If job is run standalone and use_scampcats=True, then scampcats_file cannot be ""'
+                raise IO_ValidationError(mess)
+
+            # Check for valid input scampheads_file
+            if self.mojo_execution_mode == 'job as script' and self.use_scampcats and self.scampheads_file == "":
+                mess = 'If job is run standalone and use_scampcats=True, then scamphead_file cannot be ""'
+                raise IO_ValidationError(mess)
+                
 
         # To also accept comma-separeted input lists
         def _argparse_postproc_doBANDS(self, v):
@@ -83,7 +116,10 @@ class Job(BaseJob):
         """ Pre-wash of inputs, some of these are only needed when run as script"""
 
         # Re-cast the ctx.assoc/ctx.catlist as dictionary of arrays instead of lists
-        self.ctx.catlist  = utils.dict2arrays(self.ctx.catlist)
+        self.ctx.catlist       = utils.dict2arrays(self.ctx.catlist)
+        if self.ctx.use_scampcats:
+            self.ctx.scampcatlist  = utils.dict2arrays(self.ctx.scampcatlist)
+            self.ctx.scampheadlist = utils.dict2arrays(self.ctx.scampheadlist)
 
         # Get the BANDs information in the context if they are not present
         if self.ctx.get('gotBANDS'):
@@ -108,6 +144,10 @@ class Job(BaseJob):
 
         # 2. Write the scamp input list of expcats
         self.write_scamp_input_list_file()
+
+        # Soft link the scampcat file (fits and head)
+        if self.ctx.use_scampcats:
+            self.copy_scampcat_files()
         
         # 3. Get the list of command lines
         cmdlist = self.get_scamp_cmd_list()
@@ -139,6 +179,32 @@ class Job(BaseJob):
         tableio.put_data(fh.get_expcat_list_file(tiledir, tilename_fh),(expcats,),format="%s")
         return
 
+    # Copy and rename the scampcat fits and head files
+    def copy_scampcat_files(self,clobber=True):
+        """
+        Copy/rename/soft links the scampcat input files to the align directory
+        """
+        # Sortcuts for less typing
+        tiledir     = self.input.tiledir
+        tilename_fh = self.input.tilename_fh
+        self.logger.info('Copying scampcat input file list to input folder')
+        for UNITNAME in self.ctx.unitnames:
+
+            # Scampcat fits catalogs
+            expcat_name   = fh.get_expcat_file(tiledir, tilename_fh, UNITNAME)
+            scampcat_name = self.ctx.scampcatlist['FILEPATH_LOCAL'][self.ctx.scampcatlist['UNITNAME'] == UNITNAME][0]
+            self.logger.debug("Linking:%s --> %s" % (scampcat_name,expcat_name))
+            utils.symlink_force(scampcat_name,expcat_name)
+
+            # Scampcat head catalogs --> ahead catalogs
+            exphead_name   = fh.get_expahead_file(tiledir, tilename_fh, UNITNAME)
+            scamphead_name = self.ctx.scampheadlist['FILEPATH_LOCAL'][self.ctx.scampheadlist['UNITNAME'] == UNITNAME][0]
+            self.logger.debug("Linking:%s --> %s" % (scamphead_name,exphead_name))
+            utils.symlink_force(scamphead_name,exphead_name)
+            
+        return
+
+
     # Run scamp
     # -------------------------------------------------------------------------
     def run_scamp(self,cmd_list):
@@ -146,16 +212,28 @@ class Job(BaseJob):
         Run scamp
         """
 
-        logfile = fh.get_scamp_log_file(self.input.tiledir, self.input.tilename_fh)
+        if self.ctx.use_scampcats:
+            logfile = fh.get_scamp_log_file(self.input.tiledir, self.input.tilename_fh,suffix='scamp_scampcats')
+        else:
+            logfile = fh.get_scamp_log_file(self.input.tiledir, self.input.tilename_fh,suffix='scamp')
+
         log = open(logfile,"w")
         self.logger.info("Will proceed to run scamp now:")
         self.logger.info("Will write to logfile: %s" % logfile)
         t0 = time.time()
 
+        current_cwd = os.getcwd()
+        self.logger.info("Moving to the TILEDIR directory for execution")
+        os.chdir(self.ctx.tiledir)
+        
+
         cmd  = ' '.join(cmd_list)
         self.logger.info("Executing scamp super-alignment for tile:%s" % self.ctx.tilename_fh)
         self.logger.info("%s " % cmd)
         status = subprocess.call(cmd,shell=True,stdout=log, stderr=log)
+        # Move back to current dir
+        self.logger.info("Moving back to current dir")
+        os.chdir(current_cwd)
         if status != 0:
             raise RuntimeError("\n***\nERROR while running scamp, check logfile: %s\n***" % logfile)
         self.logger.info("Total scamp time: %s" % elapsed_time(t0))
@@ -163,7 +241,7 @@ class Job(BaseJob):
         log.close()
         return
 
-    # Assemble the command
+    # Assemble the command for finalcut catalogs
     # -------------------------------------------------------------------------
     def get_scamp_cmd_list(self):
         """
@@ -177,6 +255,9 @@ class Job(BaseJob):
         # Update and Set the SWarp options 
         pars = self.get_scamp_parameter_set(**self.input.scamp_parameters)
         pars["XML_NAME"] = "%s" % fh.get_scamp_xml_file(tiledir, tilename_fh)
+
+        if self.ctx.use_scampcats:
+            pars["AHEADER_SUFFIX"] = ".ahead"
         
         # The Scamp configuration file
         if self.input.scamp_conf == '':
@@ -191,13 +272,14 @@ class Job(BaseJob):
             cmd.append("-%s %s" % (param,value))
         return cmd
 
+
     def get_scamp_parameter_set(self, **kwargs):
         """
         Set the scamp default options for all band in a tile and overwrite them
         with kwargs to this function.
         """
         checkplot_type = "astr_pixerror1d,astr_referror1d,fgroups,astr_referror2d,astr_refsysmap,distortion"
-        checkplot_name = ",".join( fh.get_scamp_plots(self.input.tiledir, self.input.tilename_fh, checkplot_type.split(',')))
+        checkplot_name = ",".join( fh.get_scamp_plots_relative(self.input.tilename_fh, checkplot_type.split(',')))
         scamp_parameters = {
             "CHECKPLOT_TYPE"  : checkplot_type,
             "CHECKPLOT_NAME"  : checkplot_name,
@@ -209,6 +291,7 @@ class Job(BaseJob):
         scamp_parameters.update(kwargs)
         return scamp_parameters
 
+
     # 'EXECUTION' FUNCTIONS
     # -------------------------------------------------------------------------
     def writeCall_scamp(self,cmd_list,mode='w'):
@@ -219,14 +302,24 @@ class Job(BaseJob):
 
         self.logger.info("Will write combine_cats call to: %s" % cmdfile)
         with open(cmdfile, mode) as fid:
+            fid.write("#!/usr/bin/env bash\n\n")
+            fid.write('# Save current pwd\n')
+            fid.write("current=`pwd`\n")
+            fid.write("cd %s\n" % self.ctx.tiledir)
             fid.write(bkline.join(cmd_list)+'\n')
             fid.write('\n\n')
+            fid.write('# Get back to prev directory\n')
+            fid.write('cd $current\n')
+            fid.write('\n\n')
+
+        os.chmod(cmdfile, 0755)
         return
 
     # -------------------------------------------------------------------------
 
     def __str__(self):
         return 'Creates the call to scamp'
+
 
 if __name__ == '__main__':
     from mojo.utils import main_runner
